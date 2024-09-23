7967c4bcbaf6c86ba663ea73aa6f3062442028d3
unknown <inha.virza@gmail.com>
2024-09-24 01:59:44 +0200

    auth

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ const categoryRoutes = require('./routes/category');
 const brandRoutes = require('./routes/brand');
 const productRoutes = require('./routes/product');
 const customerRoutes = require('./routes/customer');
+const authRoutes = require('./routes/auth');
+const { verifyToken, isAdmin } = require('./db/middleware/auth-middleware');
 
 app.use(cors());
 app.use(express.json());
@@ -16,10 +18,11 @@ app.get('/', (req, res) => {
   res.send('Server runnings');
 });
 
-app.use('/category', categoryRoutes);
-app.use('/brand', brandRoutes);
-app.use('/product', productRoutes);
-app.use('/customer', customerRoutes);
+app.use('/category', verifyToken, isAdmin, categoryRoutes);
+app.use('/brand', verifyToken, isAdmin, brandRoutes);
+app.use('/product', verifyToken, isAdmin, productRoutes);
+app.use('/customer', verifyToken, customerRoutes);
+app.use('/auth', authRoutes);
 
 mongoose
   .connect(keys.mongoURI)
